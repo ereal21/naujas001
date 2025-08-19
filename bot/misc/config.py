1034d8aca0631df731c673bf32d99556bcf89129
ereal21 <erikaskukuruzas@gmail.com>
2025-08-19 12:21:36 +0300

    Add files via upload

---
+++ bot/misc/config.py
@@ -0,0 +1,16 @@
+from abc import ABC
+from typing import Final
+
+
+class TgConfig(ABC):
+    STATE: Final = {}
+    BLACKJACK_STATS: Final = {}
+    CHANNEL_URL: Final = 'https://t.me/NBAXSHOP'
+    HELPER_URL: Final = '@SugarFromShef'
+    REVIEWS_URL: Final = 'https://t.me/+-yPLEiJwp-IxYzlk'
+    PRICE_LIST_URL: Final = 'https://t.me/NBAXSHOP_price'
+    GROUP_ID: Final = -988765433
+    REFERRAL_PERCENT = 5
+    PAYMENT_TIME: Final = 1800
+    RULES: Final = 'insert your rules here'
+    START_PHOTO_PATH: Final = r'C:\\Users\\eriku\\Downloads\\photo_2025-08-17_23-09-19.jpg'
